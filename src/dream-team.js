b95ed75a6b692669fca8f6afe6e2393f013821b4
jnkdi <anaidik11@gmail.com>
2021-03-29 19:39:20 +0300

    add: solution to dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
-const CustomError = require("../extensions/custom-error");
+// 1. Проверка на тип данных
+// 2. Проходка по элементам массива
+// 3. Добавление первого элемента строки к массиву ИМЯ
+// 4. Отсортировать массив имя и вывести его
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!members){
+    return false;
+  }
+  let name = [];
+  for(let i = 0; i < members.length; i++){
+    if(typeof members[i] === 'string'){
+      name.push(members[i].trim().toUpperCase()[0]);
+    }
+  }
+  return name.sort().join('');
 };
